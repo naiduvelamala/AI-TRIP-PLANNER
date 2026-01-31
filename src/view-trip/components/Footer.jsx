c4187393b358bb33248b18a4e9d3fb30461913c4
Rama Murthy Naidu <naiduvelamala17@gmail.com>
2026-01-31 13:57:43 +0530

    Initial commit: AI Trip Planner with Gemini and Firebase

---
+++ src/view-trip/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer({ trip }) {
+    return (
+        <div className='my-5'>
+            <h2 className='text-center text-gray-500'>Created by Suraj Paul (AI Travel Planner App)</h2>
+
+        </div>
+    )
+}
+
+export default Footer;
